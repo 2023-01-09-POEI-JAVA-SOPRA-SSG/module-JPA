87ae45a0837b94c4fd93e1a85daa1bb4509e22df
AllanBernier <allan.bernier1@gmail.com>
2023-02-15 15:32:05 +0100

    Table per class

--- Heritage/src/main/java/service/Console.java
+++ Heritage/src/main/java/service/Console.java
@@ -9,6 +9,7 @@ import javax.persistence.Persistence;
 import JOINED.JPayment;
 import JOINED.JVehicule;
 import SINGLE_TABLE.DVehicule;
+import TABLE_PER_CLASS.TVehicule;
 
 public class Console {
 	public static void main(String[] args) {
@@ -21,30 +22,28 @@ public class Console {
 			em = emf.createEntityManager();
 //			
 			
-//			System.out.println("========= SINGLE TABLE =============");
-//			
-//			List<DVehicule> vehicules = em.createQuery("from DVehicule", DVehicule.class).getResultList();
-//			
-//			for (DVehicule dVehicule : vehicules) {
-//				System.out.println(dVehicule);
-//			}
-//			
-//			System.out.println("========= JOINED =============");
-//
-//			List<JVehicule> jvehicules = em.createQuery("from JVehicule", JVehicule.class).getResultList();
-//			
-//			for (JVehicule jVehicule : jvehicules ) {
-//				System.out.println(jVehicule);
-//			}
-//			
+			System.out.println("========= SINGLE TABLE =============");
 			
+			List<DVehicule> vehicules = em.createQuery("from DVehicule", DVehicule.class).getResultList();
 			
+			for (DVehicule dVehicule : vehicules) {
+				System.out.println(dVehicule);
+			}
 			
+			System.out.println("========= JOINED =============");
+
+			List<JVehicule> jvehicules = em.createQuery("from JVehicule", JVehicule.class).getResultList();
 			
-			List<JPayment> payment = em.createQuery("From JPayment", JPayment.class).getResultList();
+			for (JVehicule jVehicule : jvehicules ) {
+				System.out.println(jVehicule);
+			}
 			
-			for (JPayment dPayment : payment) {
-				System.out.println(dPayment);
+			System.out.println("========= TABLE PER CLASS =============");
+
+			List<TVehicule> tvehicules = em.createQuery("From TVehicule", TVehicule.class).getResultList();
+			
+			for (TVehicule vehicule : tvehicules) {
+				System.out.println(vehicule);
 			}
 			
 			
